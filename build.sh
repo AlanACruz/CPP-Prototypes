4ffb3476c2ed80f7740cc173fc922373365baad0
Alan Cruz <cruz.alan@gmail.com>
2024-07-15 22:22:48 -0400

    typo

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@ cd ~/git/CPP-Prototypes
 mkdir build
 cd build
 
-conan install ..
-
 # https://docs.conan.io/1/getting_started.html
-conan profile update settings.compiler.libcxx=libstdc++11 default
+conan profile new default --detect  # Generates default profile detecting GCC and sets old ABI
+conan profile update settings.compiler.libcxx=libstdc++11 default  # Sets libcxx to C++11 ABI
+
+conan install ..
 
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
