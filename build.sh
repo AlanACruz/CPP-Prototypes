2a32e57c06f3abdda22895656f515e04c7fd6e88
Alan Cruz <cruz.alan@gmail.com>
2024-07-15 22:35:31 -0400

    fix build missing

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 cd ~/git/CPP-Prototypes
 
-mkdir build
+mkdir -p build
 cd build
 
 # https://docs.conan.io/1/getting_started.html
 conan profile new default --detect  # Generates default profile detecting GCC and sets old ABI
 conan profile update settings.compiler.libcxx=libstdc++11 default  # Sets libcxx to C++11 ABI
 
-conan install ..
+conan install .. --build=missing
 
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
