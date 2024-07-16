4c5ee3c185432fc8c766c82d2f3b18cf1ca2f8a9
Alan Cruz <cruz.alan@gmail.com>
2024-07-15 22:18:31 -0400

    typo

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ cd ~/git/CPP-Prototypes
 mkdir build
 cd build
 
+conan install ..
+
 # https://docs.conan.io/1/getting_started.html
 conan profile update settings.compiler.libcxx=libstdc++11 default
-
-conan install ..
 
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
 # https://docs.conan.io/1/creating_packages/getting_started.html
-conan build
+conan build .
